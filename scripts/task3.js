f91ebca5d9066f7a0cb8e3f5fa96bcd5c6bc6708
Alexsander <56883568+AlexsanderMr@users.noreply.github.com>
2020-03-01 13:56:50 +0200

    Add files via upload

---
+++ scripts/task3.js
@@ -0,0 +1,4 @@
+const ME = 15
+const  FRIEND = 14
+alert  (ME + FRIEND);
+console.log  (108 + 223 - 2 * 5);
---
+++ scripts/task4.js
@@ -0,0 +1,7 @@
+const NUMBER1 = 5
+const NUMBER2 = 3 
+let a1 = (NUMBER1 + NUMBER2);
+let a2 = (NUMBER1 - NUMBER2);
+let a3 = (NUMBER1 * NUMBER2);
+let a4 = (NUMBER1 / NUMBER2);
+let a5 = (a1 + a2 + a3 + a4);
